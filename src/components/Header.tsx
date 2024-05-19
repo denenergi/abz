f63763b1c123535b15ede70df92c7709085137ed
Denys <denenergii@gmail.com>
2024-05-19 20:20:08 +0300

    gh-create

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { useAppDispatch } from '../store/hooks';
 import { login, logout } from '../store/user/userSlice';
